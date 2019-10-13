6da759d3324f37ef3429bcd640431374d1a19527
Aleksey <ale612@bk.ru>
2019-10-13 18:39:37 +0300

    corrected

--- StudentMarks/Program.cs
+++ StudentMarks/Program.cs
@@ -1,70 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StudentMarks
 {
     class Mark
     {
         public string subject;
-        public int mark;
-        public Mark (string s)
+        public List<int> mark=new List<int>();
+        public Mark (string s) {subject = s;}
+        public float GetAvgSubMark()//Средние оценки по предмету
         {
-            subject = s;           
-        }
-        public void GetMark()
-        {
-            do
-            {
-                Console.Write($"{subject} mark is ");
-                mark = Convert.ToInt32(Console.ReadLine());
-            }
-            while ((5 < mark) | (mark < 0));
+            int mult = 0; int r = 0;float savg = 0;            
+            for (int i = 0; i < mark.Count-1; i++){ mult += mark[i];r++; }
+            if (r==0) { r = 1; }//если список пустой, на 0 не делим
+            savg =(float) mult / r;
+            return savg;
         }
     }
     class Student
     {
-        public string Name;
-        public Mark[] marks = new Mark[4];        
-        public Student(string n)
+        public string Name;        
+        public Student(string n) { Name = n; }
+        public List<Mark> SubM=new List<Mark>();       
+        
+        public float GetAvgMark()//Средняя оценка студента
         {
-            Name = n;           
-            marks[0] = new Mark("mathematics");
-            marks[1] = new Mark("phisics");
-            marks[2] = new Mark("Chemistry");
-            marks[3] = new Mark("Biology");
-        }
-        public float GetAvgMark()
-        {
-            int mult = 0;
-            for(int i=0;i<4;i++)
-            {
-                mult += marks[i].mark;
-            }
-            float avg =(float) mult / 4;
+            int mult = 0;int r = 0;float avg = 0;
+            for(int i=0;i<SubM.Count;i++)
+                for(int j=0;j<SubM[i].mark.Count-1;j++)
+                { mult += SubM[i].mark[j];r++; }
+            if (r == 0) { r = 1; }//если список пустой
+            avg =(float) mult / r;
             return avg;
-        }
+        }        
         public void ResetAllMarks()
         {
+            Console.WriteLine();
             Console.Write($"Do you want reset {Name} marks? (y/n) ");
             string r = Console.ReadLine();
             if (r == "n"){ return; }
-            for (int i = 0; i < 4; i++) { marks[i].mark = 0; }
+            for(int i = 0; i < SubM.Count; i++) { SubM[i].mark.Clear(); }
         }
         public void GetMarksTable()
-        {
-            Console.WriteLine($"Enter {Name} marks");
-            for (int i = 0; i < 4; i++)
+        {               
+            Mark c;
+            do
             {
-                marks[i].GetMark();
+                Console.Write($"Enter {Name} subject (enter \"0\" to stop subjects list) ");
+                c =new Mark( Console.ReadLine());
+                if (c.subject == "0") { break; }
+                Console.WriteLine($"Enter {Name} marks on {c.subject} (enter \"0\" to stop marks list)");
+                do
+                {
+                    string entm = Console.ReadLine();
+                    if(int.TryParse(entm,out int entz) & (entz < 6) & (entz > -1))//Проверка корректности ввода оценки
+                    {
+                        c.mark.Add(entz);
+                        if (entz == 0) { break; }                        
+                    }
+                    else { Console.WriteLine($"The mark should be number from 1 to 5. Try again.");continue; }                    
+                }
+                while (true);
+                SubM.Add(c);
             }
+            while (true);            
         }
         public void DisplayMarksTable()
         {
-            Console.WriteLine($"---Marks table:---");
-            for (int i = 0; i < 4; i++)
+            Console.WriteLine();
+            Console.WriteLine($"---{Name} Marks table:---");
+            for (int i = 0; i < SubM.Count; i++)
             {
-                Console.WriteLine($"{Name} mark in {marks[i].subject} is {marks[i].mark}");
+                Console.Write($"-{SubM[i].subject}- :");
+                for (int j = 0; j < SubM[i].mark.Count-1; j++)
+                {
+                    Console.Write($" {SubM[i].mark[j]} ");
+                }
+                Console.WriteLine($"  --Average of {SubM[i].subject} is: {SubM[i].GetAvgSubMark()}");
             }
-            Console.WriteLine($"Average of all marks {GetAvgMark()}");
+            Console.WriteLine();
+            Console.WriteLine($"Average of all {Name} marks is: {GetAvgMark()}");
         }
     }
     class Program
